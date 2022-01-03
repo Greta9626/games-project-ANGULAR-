06bf75d39998fba3ba83d2822cd59659f20a5d04
Greta9626 <86754253+Greta9626@users.noreply.github.com>
2022-01-03 20:15:44 +0100

    fix: Routing Homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/games', pathMatch: 'full' },
+  { path: '', redirectTo: '/games/homepage', pathMatch: 'full' },
   { path: 'games', loadChildren: () => import('./modules/games/games.module').then(m => m.GamesModule) }
 ];
 
